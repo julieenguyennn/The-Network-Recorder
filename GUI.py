7ba745df54098ed8a9977c75cc76881ce8e3edaa
Rae Zhang <junweizhang130@Gmail.com>
2023-12-02 14:35:54 -0500

    Create GUI.py

---
+++ GUI.py
@@ -0,0 +1,9 @@
+# Final Project - GUI
+#
+# Course Name: INF 452:Information Design V: Coding
+# Institution: University of Toronto
+# Instructor: Dr. Maher Elshakankiri
+# Creator: Rae Zhang
+# Created: 11/29/2023
+# Last Modified:
+
